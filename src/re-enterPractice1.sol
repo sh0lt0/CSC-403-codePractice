febbb542cccf5a5f263478d9cca3ac3fdc20cb3b
sholto <arjunc96@gmail.com>
2026-04-09 10:00:40 +0530

    Create re-enterPractice1.sol

---
+++ src/re-enterPractice1.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract EtherStore {
+    mapping(address => uint256) public balances;
+
+    function deposit() public payable {
+        balances[msg.sender] += msg.value;
+    }
+
+    function withdraw(uint256 _amount) public {
+        require(balances[msg.sender] >= _amount, "Not enough balance");
+
+        (bool sent, ) = msg.sender.call{value: _amount}("");
+        require(sent, "Failed to send Ether");
+
+        balances[msg.sender] -= _amount;
+    }
+
+    function getBalance() public view returns (uint256) {
+        return address(this).balance;
+    }
+}
