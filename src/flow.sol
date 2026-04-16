6911a0302288823ea3ea1542cf1305d25200ce00
sholto <arjunc96@gmail.com>
2026-04-16 10:59:13 +0530

    Update flow.sol

--- src/flow.sol
+++ src/flow.sol
@@ -1,13 +1,39 @@
-pragma solidity ^0.6.0;
+pragma solidity ^0.8.20;
 
 contract File{
 
-    mapping(address => uint256) public  balances;
+    mapping(address => uint256) public  balances;//0 // mapping 0xa > balance
 
-    function transfer(address _to, uint256 value) public {
-        require(balances[msg.sender] >= value);
+    uint public x = 0 ;
 
-        balances[msg.sender] -= value;
-        balances[_to] +=value;
+    function test() public returns(uint){
+      // return  x--; // x = x -1  > 0 -1 = max(uint256)
+        // underflow
     }
+
+    function test1() public returns(uint){
+       unchecked{
+       return  x--; 
+    }
+    }
+    //token minting, x = 0;
+    // vault = 0 token
+    // val = x -1;
+ //   max(uint256) 
+
+
+   /* function transfer(address _to, uint256 value) public  {
+        require(balances[msg.sender] >= value); // balance = 100 , val = 20
+
+        balances[msg.sender] -= value; // bal =100 , bal = bal -val =80
+        balances[_to] +=value; // rec.bal = rec.bal + val = 10 + 20 = 30
+
+      //  bal.msg.sender = 10
+       // val = -11
+        // val sent != -1 but max of int256
+
+
+       
+
+    }*/
 }
